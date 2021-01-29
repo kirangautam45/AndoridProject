5a5e647abd1d206ddb6b52657227a9549b84d5e7
Kiran Gautam <gautamkiran38@gmailo.com>
2021-01-29 21:53:44 +0545

    login form completed

--- app/src/main/java/com/example/onlinefurniture/entites/RegisterUser.kt
+++ app/src/main/java/com/example/onlinefurniture/entites/RegisterUser.kt
@@ -1,10 +1,8 @@
-package com.example.onlinefurniture.ui
+package com.example.onlinefurniture.entites
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.onlinefurniture.R
 
