74c56fe9fcc4279bed41bb1d6c5e5555011e1167
Kiran Gautam <gautamkiran38@gmail.com>
2021-01-29 15:45:38 +0545

    login  form

---
+++ app/src/main/java/com/example/onlinefurniture/ui/RegisterUser.kt
@@ -0,0 +1,31 @@
+package com.example.onlinefurniture.ui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.onlinefurniture.R
+
+class RegisterUser:AppCompatActivity() {
+ private lateinit var Fname: EditText
+ private lateinit var Lname: EditText
+ private lateinit var Username: EditText
+ private lateinit var Password: EditText
+ private lateinit var Confirmpassword: EditText
+ private lateinit var btnAddUser: Button
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+  setContentView(R.layout.add_user)
+  Fname = findViewById(R.id.Fname)
+  Lname = findViewById(R.id.Lname)
+  Username = findViewById(R.id.Username)
+  Password = findViewById(R.id.Password)
+  Confirmpassword = findViewById(R.id.ConfirmPassword)
+  btnAddUser = findViewById(R.id.btnAddUser)
+
+
+
+ }
+}
